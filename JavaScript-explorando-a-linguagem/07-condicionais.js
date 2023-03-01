a4c2929060056721dab484d986bcbf618fe3d5b3
naylaprilla <nayla.priscila@gmail.com>
2023-03-01 10:11:18 -0300

    exercicio JS Concicionais

--- JavaScript-explorando-a-linguagem/07-condicionais.js
+++ JavaScript-explorando-a-linguagem/07-condicionais.js
@@ -6,12 +6,14 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 )
 
-const idadeComprador = 15
-const estaAcompanhada = true
+const idadeComprador = 21
+const estaAcompanhada = false
+const temPassagemComprada = true
+
 console.log('Destinos Possíveis: ')
 console.log(listaDeDestinos)
 
-if (idadeComprador >= 18) {
+/* if (idadeComprador >= 18) {
     console.log('Comprador maior de idade')
     listaDeDestinos.splice(1,1)
 } else if(estaAcompanhada) {
@@ -19,9 +21,21 @@ if (idadeComprador >= 18) {
         listaDeDestinos.splice(1,1) //removendo item da lista
     }else {
         console.log('Não é maior de idade e não posso vender')   
-    }
+    } */
 
+ if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log('Boa Viagem!!!')
+    listaDeDestinos.splice(2,1)
+}else{
+    console.log('Não é maior de idade e não posso vender')   
+}
+console.log('Embarque: \n\n')
 
+if (idadeComprador > 18 && temPassagemComprada) {
+    console.log('Boa viagem!!!')
 
+} else {
+    console.log('Você não pode embarcar')
+}
 
 console.log(listaDeDestinos)
