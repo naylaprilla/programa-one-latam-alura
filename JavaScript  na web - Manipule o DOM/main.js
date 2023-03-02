43d14c6a9f38446ad08017fa1cbf09472240956b
naylaprilla <nayla.priscila@gmail.com>
2023-03-02 09:34:24 -0300

    add eventListener input e button form

--- JavaScript  na web - Manipule o DOM/main.js
+++ JavaScript  na web - Manipule o DOM/main.js
@@ -1,5 +1,9 @@
 const novaTarefa = document.querySelector('[data-form-button]')
 
+const input = document.querySelector('[data-form-input]')
+
+console.log(input)
+
 novaTarefa.addEventListener('click', ()=> {console.log('Fui clicado')})
 
 
