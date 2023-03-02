a02cf3c77dbb2156d54cb47ead0b10fc0c4dbf27
naylaprilla <nayla.priscila@gmail.com>
2023-03-02 09:50:55 -0300

    add const criarTarefa

--- JavaScript  na web - Manipule o DOM/main.js
+++ JavaScript  na web - Manipule o DOM/main.js
@@ -1,9 +1,14 @@
+const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    console.log(valor)
+    input.value = " "
+}
+
 const novaTarefa = document.querySelector('[data-form-button]')
 
-const input = document.querySelector('[data-form-input]')
-
-console.log(input)
-
-novaTarefa.addEventListener('click', ()=> {console.log('Fui clicado')})
+novaTarefa.addEventListener('click', ()=> {})
 
 
