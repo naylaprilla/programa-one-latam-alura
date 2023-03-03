0c6bf950cb121dc833a8e7d6b83ba0593849cbad
naylaprilla <nayla.priscila@gmail.com>
2023-03-03 09:24:27 -0300

    add btn deletarTarefa

--- JavaScript  na web - Manipule o DOM/main.js
+++ JavaScript  na web - Manipule o DOM/main.js
@@ -15,6 +15,7 @@
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
     lista.appendChild(tarefa)
     input.value = " "
 }
@@ -41,6 +42,26 @@ const concluirTarefa = (evento) => {
 
     tarefaCompleta.classList.toggle('done')
 
+}
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button')
+
+    botaoDeleta.innerText = 'deletar'
+    botaoDeleta.addEventListener('click', deletarTarefa)
+
+    return botaoDeleta
+
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target //pega o evento
+
+    const tarefaCompleta = botaoDeleta.parentElement //pega o elemento pai
+
+    tarefaCompleta.remove()
+
+    return botaoDeleta
 }
 })()
 
