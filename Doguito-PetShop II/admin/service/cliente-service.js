fc9b3755c94380dcf5e55c48974141091ba29d91
naylaprilla <nayla.priscila@gmail.com>
2023-03-23 09:49:57 -0300

    Revisão estudo

--- Doguito-PetShop II/admin/service/cliente-service.js
+++ Doguito-PetShop II/admin/service/cliente-service.js
@@ -2,7 +2,10 @@
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
     .then( resposta => {
-        return resposta.json()
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível listar os clientes')
     })
 }
 
@@ -19,13 +22,58 @@ const criaCliente = (nome, email) => {
         
     })
     .then( resposta => {
-        return resposta.body
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar um cliente')
+    })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }).then(resposta => {
+        if(!resposta.ok) {
+            throw new Error('Não foi possível remover um clientes')
+        }
+    })
+}
+
+const detalhaCliente = (id)=> {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then( resposta => {
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível detalhar o cliente')
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        }) 
+    })
+    .then( resposta => {
+        if(resposta.ok) {
+            return resposta.json()
+        }
+        throw new Error('Não foi possível atualizar um cliente')
     })
 }
 
 export const clienteService = {
     listaClientes,
-    criaCliente
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
 
 
